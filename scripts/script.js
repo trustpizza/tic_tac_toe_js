8568f1c3ae9b206d69ee9efebcc01b7a2c8c52b9
trustpizza <maxelolsson@gmail.com>
2023-01-19 18:24:35 -0600

    Available positions returns all available positions left

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,27 @@
 const  GameBoard = (() => {
   // "use strict";
   
-  let boardArray = Array.new(8);
-  // Return the location o
+  let boardArray = Array(9).fill(null);
+  // Needs and Purposes of the GmaeBoard:
+  // House an array of 9 items
+  // Check if a spot is empty / Check which spots are available
+  // Place a piece 
+  // Check if
+
+  let available_positions = () => {
+    out = []
+    for (let i = 0; i < boardArray.length; i++) {
+      let element = boardArray[i];
+
+      if (element === null) out.push(i);
+    }
+
+    return out;
+  }
 
   return {
     boardArray,
+    available_positions
   };
 
 })();
